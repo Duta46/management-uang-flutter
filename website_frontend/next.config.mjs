eb6ad0b930a14e1d53e909c7c7b6743a9cbf73fd
Duta <dutaalif.20091@mhs.unesa.ac.id>
2026-01-25 08:57:00 +0700

    add website frontend & licence

--- website_frontend/next.config.mjs
+++ website_frontend/next.config.mjs
@@ -1,5 +1,4 @@
-<?php
-/**
+/*
  * Copyright (c) 2026 Duta Alif Gunawan
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -21,41 +20,9 @@
  * SOFTWARE.
  */
 
-namespace App\Models;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+};
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\SoftDeletes;
-
-class Category extends Model
-{
-    use HasFactory, SoftDeletes;
-
-    protected $fillable = [
-        'user_id',
-        'name',
-    ];
-
-    protected $casts = [
-        'created_at' => 'datetime',
-        'updated_at' => 'datetime',
-    ];
-
-    protected $hidden = [
-        'user_id',
-        'created_at',
-        'updated_at',
-    ];
-
-    public function user(): BelongsTo
-    {
-        return $this->belongsTo(User::class);
-    }
-
-    public function transactions(): HasMany
-    {
-        return $this->hasMany(Transaction::class);
-    }
-}
+export default nextConfig;
